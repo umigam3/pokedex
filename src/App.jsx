e2c19435f75574a7ec56aa483ea7c569ce2f0b4f
Gerard <gerarddoncel41@gmail.com>
2024-03-26 21:07:44 +0100

    pokemon entry improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from './components/Home/Home.jsx';
 import PokemonEntry from './components/PokemonEntry/PokemonEntry.jsx';
 import styles from './App.module.css';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const DarkModeContext = createContext({});
 
@@ -10,15 +10,33 @@ function App() {
   const [isPokemonShowing, setIsPokemonShowing] = useState(false);
   const [displayPokemonData, setDisplayPokemonData] = useState({});
 
+  useEffect(() => {
+    if (isPokemonShowing) {
+      // Calculate scrollbar width
+      const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+      // Block scroll when modal is open and maintain scrollbar width
+      document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    } else {
+      // Restore scroll when modal is closed
+      document.body.style.overflow = 'auto';
+      document.body.style.paddingRight = '0';
+    }
+  }, [isPokemonShowing]);
+  
   const toggleDarkMode = () => {
     setIsDarkMode(prevMode => !prevMode);
   };
+
+  const closeModal = () => {
+    setIsPokemonShowing(false);
+  }
 
   return <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
     <div className={`${styles.container} ${isDarkMode ? styles.darkMode : styles.lightMode}`}>
       <Home isPokemonShowing={isPokemonShowing} setIsPokemonShowing={setIsPokemonShowing} displayPokemonData={displayPokemonData} setDisplayPokemonData={setDisplayPokemonData}/>
       {isPokemonShowing && (
-      <div className={styles.modalBackground}>
+      <div className={styles.modalBackground} onClick={closeModal}>
         <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
           <PokemonEntry pokemonDataToShow={displayPokemonData} isPokemonShowing={isPokemonShowing} setIsPokemonShowing={setIsPokemonShowing}/>
         </div>
