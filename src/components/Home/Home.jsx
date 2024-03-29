033df6f6e2c638cc06eb7a638f8f221fe2761b14
Gerard Doncel <gerarddoncel41@gmail.com>
2024-03-29 22:20:31 +0100

    select by type added, bug combos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,6 +10,7 @@ const Home = ({ isPokemonShowing, setIsPokemonShowing, displayPokemonData, setDi
   const [selectedType, setSelectedType] = useState(0);
   const [showedPokemonData, setShowedPokemonData] = useState([]);
   const [filteredPokemonData, setFilteredPokemonData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getAllPokemonData = async () => {
@@ -24,6 +25,7 @@ const Home = ({ isPokemonShowing, setIsPokemonShowing, displayPokemonData, setDi
       } catch (error) {
         console.error('Error fetching data:', error);
       }
+
     };
 
     getAllPokemonData();
@@ -37,21 +39,25 @@ const Home = ({ isPokemonShowing, setIsPokemonShowing, displayPokemonData, setDi
   }, [allPokemonData, searchQuery]);
 
   useEffect(() => {
-    const getAllPokemonDataByType = async () => {
-      try {
-        const response = await fetch(`https://pokeapi.co/api/v2/type/${selectedType}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
+    if (selectedType != 0) {
+      const getAllPokemonDataByType = async () => {
+        try {
+          const response = await fetch(`https://pokeapi.co/api/v2/type/${selectedType}`);
+          if (!response.ok) {
+            throw new Error('Failed to fetch data');
+          }
+          const data = await response.json();
+      
+          setFilteredPokemonData(data.pokemon.map(pokemon => pokemon.pokemon));
+        } catch (error) {
+          console.error('Error fetching data:', error);
         }
-        const data = await response.json();
-    
-        setFilteredPokemonData(data.pokemon);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+      };
 
-    getAllPokemonDataByType();
+      getAllPokemonDataByType();
+    } else {
+      setFilteredPokemonData(allPokemonData);
+    }
   }, [selectedType]);
 
   useEffect(() => {
@@ -66,20 +72,38 @@ const Home = ({ isPokemonShowing, setIsPokemonShowing, displayPokemonData, setDi
     setShowedPokemonData(prevData => [...prevData, ...nextPokemonData]);
   }
 
+  useEffect(() => {
+    setIsLoading(true);
+  
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery, selectedType]);
+
   return (
     <main className={styles.container}>
       <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} selectedType={selectedType} setSelectedType={setSelectedType} />
-      <section className={styles.cardContainer}>
-        {
-          showedPokemonData?.map((pokemon, index) => (
-            <PokemonCard key={index} pokemonUrl={pokemon.url} isPokemonShowing={isPokemonShowing} setIsPokemonShowing={setIsPokemonShowing} displayPokemonData={displayPokemonData} setDisplayPokemonData={setDisplayPokemonData}/>
-          ))
-        }
-      </section>
-      {showedPokemonData.length < filteredPokemonData.length && (
+      {isLoading ? (
         <div className={styles.loadingContainer}>
-          <button className={`${styles.loadMore} ${styles.shine}`} onClick={handleShowMoreClick}>Load more Pokémon</button>
+          Loading...
         </div>
+      ) : (
+        <>
+          <section className={styles.cardContainer}>
+            {
+              showedPokemonData?.map((pokemon, index) => (
+                <PokemonCard key={index} pokemonUrl={pokemon.url} isPokemonShowing={isPokemonShowing} setIsPokemonShowing={setIsPokemonShowing} displayPokemonData={displayPokemonData} setDisplayPokemonData={setDisplayPokemonData}/>
+              ))
+            }
+          </section>
+          {showedPokemonData.length < filteredPokemonData.length && (
+            <div className={styles.loadingContainer}>
+              <button className={`${styles.loadMore} ${styles.shine}`} onClick={handleShowMoreClick}>Load more Pokémon</button>
+            </div>
+          )}
+        </>
       )}
     </main>
   );
